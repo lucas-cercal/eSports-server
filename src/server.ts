46cac3a2aedca0163d9469c01550f8b4815b8074
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-11 16:16:03 -0300

    Finalizando rota GET de ads dos games

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ app.get('/games', async (request, response) => {
       }
     }
   })
+
   return response.json(games)
 })
 
@@ -21,15 +22,27 @@ app.post('/ads', (request, response) => {
   return response.status(201).json([])
 })
 
-app.get('/games/:id/ads', (request, response) => {
-  // const gameId = request.params.id
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true
+    },
+    where: {
+      gameId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
 
-  return response.json([
-    { id: 1, name: 'Anúncio 1' },
-    { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' },
-    { id: 4, name: 'Anúncio 4' }
-  ])
+  return response.json(ads)
 })
 
 app.get('/ads/:id/discord', (request, response) => {
