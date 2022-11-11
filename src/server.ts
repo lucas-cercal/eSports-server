7526b3f32139745c1f889bd3a25f53adf5dda6d9
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-11 16:26:39 -0300

    Adicionando rota GET para obter ad

--- src/server.ts
+++ src/server.ts
@@ -50,10 +50,20 @@ app.get('/games/:id/ads', async (request, response) => {
   }))
 })
 
-app.get('/ads/:id/discord', (request, response) => {
-  // const adId = request.params.id
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true
+    },
+    where: {
+      id: adId
+    }
+  })
 
-  return response.json([])
+  return response.json({
+    discord: ad.discord
+  })
 })
 
 app.listen(3333)
