79a1f20dc492c87346f665b60c284b05b4526e5d
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-11 17:19:36 -0300

    Adicionando util convert hour string to minutes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { PrismaClient } from '@prisma/client'
 
 const app = express()
+app.use(express.json())
+
 const prisma = new PrismaClient()
 
 app.get('/games', async (request, response) => {
@@ -18,8 +20,26 @@ app.get('/games', async (request, response) => {
   return response.json(games)
 })
 
-app.post('/ads', (request, response) => {
-  return response.status(201).json([])
+app.post('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id
+  const body = request.body
+
+  // const [hour.]
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: body.hourStart,
+      hourEnd: body.hourEnd,
+      useVoiceChannel: body.useVoiceChannel
+    }
+  })
+
+  return response.status(201).json(body)
 })
 
 app.get('/games/:id/ads', async (request, response) => {
